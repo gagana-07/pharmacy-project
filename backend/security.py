1f8fd9359de95d34aef6893986748ad20e70c30f
gagana-07 <07.gaganasomashekar@gmailcom>
2026-09-10 21:16:10 +0530

    Added company registration, user approval workflow and bcrypt authentication

---
+++ backend/security.py
@@ -0,0 +1,17 @@
+from jose import jwt
+from datetime import datetime, timedelta
+
+SECRET_KEY = "pharmacy-secret-key"
+ALGORITHM = "HS256"
+
+def create_access_token(data):
+
+    payload = data.copy()
+
+    payload["exp"] = datetime.utcnow() + timedelta(hours=24)
+
+    return jwt.encode(
+        payload,
+        SECRET_KEY,
+        algorithm=ALGORITHM
+    )
